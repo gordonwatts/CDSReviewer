731dccb69885df3ff3c70ef9603a9fa4ca6438e5
Gordon Watts <gordon@gordonandpaula.net>
2013-12-27 00:49:32 -0500

    Very first code to do the internal database storage on the phone and store.

---
+++ CDSReviewerCore/Data/PaperFullInfo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CDSReviewerCore.Data
+{
+    /// <summary>
+    /// The full information for a paper - excluding everything that is in the stub info.
+    /// </summary>
+    public class PaperFullInfo
+    {
+        /// <summary>
+        /// Paper abstract
+        /// </summary>
+        public string Abstract { get; set; }
+
+        /// <summary>
+        /// List of authors
+        /// </summary>
+        public string[] Authors { get; set; }
+    }
+}
---
+++ CDSReviewerCoreTest/PaperDB/IsolatedStorageDBTest.cs
@@ -0,0 +1,40 @@
+﻿using CDSReviewerCore.Data;
+using CDSReviewerCore.PaperDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace CDSReviewerCoreTest.PaperDB
+{
+    [TestClass]
+    public class IsolatedStorageDBTest
+    {
+        [TestMethod]
+        public async Task StoreRetreiveSingleValue()
+        {
+            IInternalPaperDB paperdb = new IsolatedStorageDB();
+            var p = CreatePaperInfo("CDS1234");
+            await paperdb.Add(p.Item1, p.Item2);
+            var info = await paperdb.GetFullInfoForID(p.Item1.ID);
+            Assert.IsNotNull(info, "getting back the item");
+            Assert.AreEqual(p.Item2.Abstract, info.Abstract, "Abstract");
+        }
+
+        /// <summary>
+        /// Create a paper for testing use.
+        /// </summary>
+        /// <param name="paperID"></param>
+        /// <returns></returns>
+        private Tuple<PaperStub, PaperFullInfo> CreatePaperInfo(string paperID)
+        {
+            var ps = new PaperStub() { ID = paperID, Title = string.Format("Paper title for {0}.", paperID) };
+            var pf = new PaperFullInfo()
+            {
+                Abstract = string.Format("Abstract of paper for {0}.", paperID),
+                Authors = new string[] { "A. Berson", "C. Duderson", "E. Fillerson" }
+            };
+
+            return Tuple.Create(ps, pf);
+        }
+    }
+}
