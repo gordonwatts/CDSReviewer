ac5b1d2943dfd49d47a74a60ec188a159e2b8463
Gordon Watts <gordon@gordonandpaula.net>
2014-03-01 02:08:54 -0800

    Home page with a button to go to the add page

---
+++ CDSReviewerModels/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using Caliburn.Micro.Portable;
+
+namespace CDSReviewerModels.ViewModels
+{
+    /// <summary>
+    /// The home page view model
+    /// </summary>
+    public class HomePageViewModel : Screen
+    {
+        /// <summary>
+        /// Track the navagation service we will be using to move around the app.
+        /// </summary>
+        private readonly INavService _nav;
+
+        /// <summary>
+        /// Initialize with the navigation service.
+        /// </summary>
+        /// <param name="nav"></param>
+        public HomePageViewModel(INavService nav)
+        {
+            _nav = nav;
+        }
+
+        /// <summary>
+        /// Move away to the add new document view.
+        /// </summary>
+        public void CmdAdd()
+        {
+            _nav.NavigateToViewModel<AddCDSPaperViewModel>();
+        }
+    }
+}
---
+++ CDSReviewerModelsTest/ViewModels/HomePageViewModelTest.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro.Portable;
+using CDSReviewerModels.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CDSReviewerModelsTest.ViewModels
+{
+    [TestClass]
+    public class HomePageViewModelTest
+    {
+        [TestMethod]
+        public void TestCTor()
+        {
+            INavService obj = Mock.Of<INavService>();
+            var h = new HomePageViewModel(obj);
+        }
+
+        [TestMethod]
+        public void TestAddButtonHit()
+        {
+            var moqObj = new Mock<INavService>(MockBehavior.Loose);
+
+            var h = new HomePageViewModel(moqObj.Object);
+            h.CmdAdd();
+
+            moqObj.Verify(n => n.NavigateToViewModel<AddCDSPaperViewModel>());
+        }
+    }
+}
