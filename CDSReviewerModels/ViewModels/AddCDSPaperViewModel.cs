e100fec6dd104ddf15a31bbc4a6e212c20493432
Gordon Watts <gordon@gordonandpaula.net>
2014-03-02 01:56:56 -0800

    Add back button functionality to the WS app

--- CDSReviewerModels/ViewModels/AddCDSPaperViewModel.cs
+++ CDSReviewerModels/ViewModels/AddCDSPaperViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Caliburn.Micro;
-using Caliburn.Micro.Portable;
+﻿using Caliburn.Micro.Portable;
 
 namespace CDSReviewerModels.ViewModels
 {
@@ -7,8 +6,16 @@ namespace CDSReviewerModels.ViewModels
     /// View model for adding a paper to the archive. Takes care of look up and
     /// verification after user enters a "search" string.
     /// </summary>
-    public class AddCDSPaperViewModel : Screen
+    public class AddCDSPaperViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Pass down the navagation service
+        /// </summary>
+        /// <param name="nav"></param>
+        public AddCDSPaperViewModel(INavService nav)
+            : base(nav)
+        { }
+
         /// <summary>
         /// Get/Set the CDS lookup string. Used to do the search for the document.
         /// - Currently supports only a URL.
---
+++ CDSReviewerModels/ViewModels/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using Caliburn.Micro.Portable;
+
+namespace CDSReviewerModels.ViewModels
+{
+    /// <summary>
+    /// Base class for common items for our view models.
+    /// </summary>
+    public class ViewModelBase : Screen
+    {
+        private INavService _nav;
+        public ViewModelBase(INavService nav)
+        {
+            _nav = nav;
+        }
+
+        public void GoBack()
+        {
+            _nav.GoBack();
+        }
+
+        public bool CanGoBack
+        {
+            get { return _nav.CanGoBack; }
+        }
+    }
+}
