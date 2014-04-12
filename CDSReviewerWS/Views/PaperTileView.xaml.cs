edf28ea0fcf33f6ead2679718f75f2a234e60ebf
Gordon Watts <gordon@gordonandpaula.net>
2014-04-11 22:42:14 -0700

    Missed papertileview items for the WS app

---
+++ CDSReviewerWS/Views/PaperTileView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.Graphics.Display;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CDSReviewerWS.Views
+{
+    public sealed partial class PaperTileView : UserControl
+    {
+        public PaperTileView()
+        {
+            this.InitializeComponent();
+            PaperTitle.MaxHeight = PaperTitle.FontSize * 2.0 / 72 * DisplayInformation.GetForCurrentView().LogicalDpi;
+        }
+    }
+}
