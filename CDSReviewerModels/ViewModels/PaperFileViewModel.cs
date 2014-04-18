581587f2db3d89737a6191e2c022df03ec3ecd80
Gordon Watts <gordon@gordonandpaula.net>
2014-04-18 22:51:08 +0200

    ViewModel to surface info for a paper file that can be opened, etc.

---
+++ CDSReviewerModels/ViewModels/PaperFileViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDSReviewerModels.ViewModels
+{
+    /// <summary>
+    /// Represents a paper file view model. This is meant to display a single version of a file
+    /// self contained.
+    /// </summary>
+    public class PaperFileViewModel : PropertyChangedBase
+    {
+        /// <summary>
+        /// Initialize the view model with the appropriate values
+        /// </summary>
+        /// <param name="fname"></param>
+        /// <param name="version"></param>
+        /// <param name="date"></param>
+        public PaperFileViewModel(string fname, int version, DateTime date)
+        {
+            FileName = fname;
+            Version = version;
+            FileDate = date;
+        }
+
+        /// <summary>
+        /// Get the filename that we represent
+        /// </summary>
+        public string FileName {get; private set;}
+
+        /// <summary>
+        /// Get the filename that we represent
+        /// </summary>
+        public int Version { get; private set; }
+
+        /// <summary>
+        /// Get the date that this version was uploaded to CDS
+        /// </summary>
+        public DateTime FileDate { get; private set; }
+    }
+}
---
+++ CDSReviewerModelsTest/ViewModels/PaperFileViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CDSReviewerModels.ViewModels;
+
+namespace CDSReviewerModelsTest.ViewModels
+{
+    [TestClass]
+    public class PaperFileViewModelTest
+    {
+        /// <summary>
+        /// Make sure init works
+        /// </summary>
+        [TestMethod]
+        public void TestInit()
+        {
+            var f = new PaperFileViewModel("fName", 22, DateTime.Parse("July 1, 2011"));
+            Assert.AreEqual("fName", f.FileName);
+            Assert.AreEqual(22, f.Version);
+            Assert.AreEqual(DateTime.Parse("July 1, 2011"), f.FileDate);
+        }
+    }
+}
