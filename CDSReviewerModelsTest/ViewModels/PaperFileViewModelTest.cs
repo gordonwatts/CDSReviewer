2b0b5d254f724eb52b49ad9ac9540719777c41c9
Gordon Watts <gordon@gordonandpaula.net>
2014-05-06 06:09:27 -0700

    Adding some tests to help track the download behavior

--- CDSReviewerModelsTest/ViewModels/PaperFileViewModelTest.cs
+++ CDSReviewerModelsTest/ViewModels/PaperFileViewModelTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CDSReviewerModels.ViewModels;
 using CDSReviewerCore.Data;
+using Microsoft.Reactive.Testing;
+using ReactiveUI.Testing;
 
 namespace CDSReviewerModelsTest.ViewModels
 {
@@ -21,6 +23,40 @@ namespace CDSReviewerModelsTest.ViewModels
             Assert.AreEqual("fName", f.FileName);
             Assert.AreEqual(22, f.Version);
             Assert.AreEqual(DateTime.Parse("July 1, 2011"), f.FileDate);
+        }
+
+        [TestMethod]
+        public void FileNotThere()
+        {
+            new TestScheduler().With(shed =>
+            {
+                string id = "1234";
+                var file = new PaperFile() { FileName = "fName" };
+                var version = new PaperFileVersion() { VersionDate = DateTime.Parse("July 1, 2011"), VersionNumber = 22 };
+                var f = new PaperFileViewModel(id, file, version);
+
+                shed.AdvanceByMs(20);
+
+                Assert.Inconclusive("FIle is not downloaded");
+            }
+        }
+
+        [TestMethod]
+        public void FileDownloadStart()
+        {
+            Assert.Inconclusive("File is not downloaded, and it was started");
+        }
+
+        [TestMethod]
+        public void FileDownloaded()
+        {
+            Assert.Inconclusive("File is already downloaded");
+        }
+
+        [TestMethod]
+        public void FileDownloadFails()
+        {
+            Assert.Inconclusive("Flie download fails.");
         }
     }
 }
